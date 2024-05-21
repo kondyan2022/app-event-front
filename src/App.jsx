bcc3f4830a097456a5c3f7399920bf1e7661886d
kondyan2022 <kondyan.asusnexus@gmail.com>
2024-05-22 00:15:08 +0300

    add global loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import { Suspense } from "react";
 
 import "./App.css";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import { Container } from "./components/Container/Container";
 import Header from "./components/Header/Header";
+import { Spin } from "antd";
 
 function App() {
+  const navigation = useNavigation();
+
   return (
     <>
       <Header />
       <Container>
+        <Spin
+          tip="Loading "
+          style={{ backgroundColor: "#00000020" }}
+          spinning={navigation.state === "loading"}
+          size="large"
+          fullscreen
+        />
         <Suspense fallback={<div>...Loading</div>}>
           <Outlet />
         </Suspense>
